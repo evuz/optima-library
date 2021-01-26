1f9b6647b71d4d0beaf444eecd87d4a19289b4e4
Jesus Gomez Bellido <jesgombel@outlook.es>
2021-01-26 16:21:52 +0100

    feat: create factory and entry point

---
+++ src/factory.ts
@@ -0,0 +1,41 @@
+import { Config } from './Entities/Config'
+import { DemoMatchFactory } from './Factories/Match/DemoMatchFactory'
+import { FinishMatchService } from './Services/FinishMatchService'
+import { GetSummaryService } from './Services/GetSummaryService'
+import { MatchService } from './Services/MatchService'
+import { ScoreboardService } from './Services/ScoreboardService'
+import { RandomMathGeneratorStrategy } from './Strategies/MatchGenerator/RandomStrategy'
+import { RandomMatchResolverStrategy } from './Strategies/MatchResolver/RandomMatchResolverStrategy'
+import { FinishedMatchesUseCase } from './UseCases/FinishedMatchesUseCase'
+import { ScoreboardUseCase } from './UseCases/ScoreboardUseCase'
+import { Domain } from './Utils/Domain'
+import { spainTeams } from './Utils/TeamsGenerator'
+
+const domainMode = ['demo'] as const
+
+type DomainMode = typeof domainMode[number]
+
+export function createDomain (mode: DomainMode = 'demo') {
+  if (!domainMode.includes(mode)) {
+    throw Error(`mode ${mode} is not a valid mode`)
+  }
+
+  const config: Config = {
+    matchDuration: 3 * 60, // 3 min
+    resultsPageSize: 10
+  }
+  const matchGenerator = new RandomMathGeneratorStrategy(config)
+  const matchResolver = new RandomMatchResolverStrategy(config)
+
+  const matchFactory = new DemoMatchFactory(spainTeams(), matchGenerator, matchResolver)
+
+  const matchService = new MatchService()
+  const scoreboardService = new ScoreboardService(matchFactory)
+  const getSummaryService = new GetSummaryService(config, matchService)
+  const finishedService = new FinishMatchService(matchService, matchFactory)
+
+  return new Domain({
+    scoreboard: new ScoreboardUseCase(scoreboardService),
+    summary: new FinishedMatchesUseCase(getSummaryService, finishedService)
+  })
+}
---
+++ src/lib.ts
@@ -0,0 +1,13 @@
+/* Entities */
+export * from './Entities/Config'
+export * from './Entities/EventMatch'
+export * from './Entities/Match'
+export * from './Entities/Score'
+export * from './Entities/Team'
+
+/* Use cases */
+export * from './UseCases/FinishedMatchesUseCase'
+export * from './UseCases/ScoreboardUseCase'
+
+/* Factory */
+export * from './factory'
